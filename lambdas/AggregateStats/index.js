ead1c5ad5c924121dd9858ba1c9537b0200ae084
Gabriel Foust <gfoust@harding.edu>
2026-04-01 17:26:14 -0500

    Add CORS headers to return value

--- lambdas/AggregateStats/index.js
+++ lambdas/AggregateStats/index.js
@@ -1,8 +1,4 @@
 
 export async function handler(event) {
   // This is just a place-holder; replace it with your actual lambda function
-  return {
-    statusCode: 200,
-    body: { name: "batchProcessor" }
-  };
-};
+}
--- lambdas/SubmitSurvey/index.js
+++ lambdas/SubmitSurvey/index.js
@@ -2,7 +2,11 @@
 export async function handler(event) {
   // This is just a place-holder; replace it with your actual lambda function
   return {
-    statusCode: 200,
-    body: { name: "submitHandler" }
+  statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: "Response recorded" }),
   };
-};
+}
